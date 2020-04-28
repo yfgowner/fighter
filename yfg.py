6881b6f876aa6b44909b360243e4eefaef812324
yfgowner <64449538+yfgowner@users.noreply.github.com>
2020-04-28 04:28:55 +0000

    Update yfg.py

--- yfg.py
+++ yfg.py
@@ -99,10 +99,10 @@ while (loop == 'true'):
             loop = 'false'
         else:
             print "Wrong Password"
-            os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+            os.system('xdg-open https://www.Youtube.com/')
     else:
         print "Wrong Username"
-        os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+        os.system('xdg-open https://www.Youtube.com/')
 
 def login():
 	os.system('clear')
